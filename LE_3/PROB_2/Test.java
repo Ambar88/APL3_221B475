3ffb1e22d618726124d7ddb57cc4a0799131f5b2
221B475 <221B475@juet.co.in>
2025-02-06 14:30:07 +0530

    this is lab exercise 3

---
+++ LE_3/PROB_2/Test.java
@@ -0,0 +1,10 @@
+class Test implements Testable {
+    public void display() {
+        System.out.println("Display method in Test class.");
+    }
+
+    public static void main(String[] args) {
+        Test obj = new Test();
+        obj.display();
+    }
+}
